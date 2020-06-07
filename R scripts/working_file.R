9c2697274837e5823871c60499e9e0283d314aad
Sanmari Vivier <sanmari.vivier@gmail.com>
2020-06-07 16:03:19 +0200

    Visualise dataset

--- R scripts/working_file.R
+++ R scripts/working_file.R
@@ -1,17 +1,38 @@
-
+tidyverse_packages()
 
 # Import dataset--------------------------------
 data <- read_csv("data/ICD10_F43.2_patients.csv")
 data_tib <- as_tibble(data)
 
 # Explore data----------------------------------
-males_tib <- data_tib %>% 
-            filter(data_tib$Sex == "m") %>% 
-            glimpse()
 
-females_tib <- data_tib %>% 
-               filter(data_tib$Sex == "f") %>% 
-               glimpse()
 
 # Data wrangling-------------------------------
+data_tib <- data_tib %>% 
+            mutate(Birth_year = year(`date-of-birth`)) %>%
+            glimpse()
+
+data_tib <- data_tib %>% 
+    mutate(age = `therapy-year` - Birth_year) %>%
+    glimpse()
+
+males_tib <- data_tib %>% 
+    filter(data_tib$Sex == "m") %>% 
+    glimpse()
+
+females_tib <- data_tib %>% 
+    filter(data_tib$Sex == "f") %>% 
+    glimpse()
+
+fig <- ggplot(data = data_tib, aes(x = age, y = `total-sessions`, color = Sex)) + 
+    geom_point(alpha = 0.5) + 
+    labs(
+        title = "Visualisation of data set",
+        subtitle = "Therapy sessions according to age and sex",
+        x_lab= "Age",
+        y_lab="Sessions")+
+    theme_tq()
+
+ggplotly(fig)
+
 
